fb413e7770dacabd83be7bfbd76240e07f565809
siddurajesh <siddu.rajesh1947@gmail.com>
2022-12-10 08:34:44 -0800

    added second heading

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>This heading is added from Rajeshwar local repositary</h1>
+      <h1>This is a second heading </h1>
     </div>
   );
 }
